eb847c85639c3a299ffcee1c30ba791030aae62a
hroddric <rodrigo.perezordoyobellido@gmail.com>
2020-04-26 21:27:34 +0200

    Finished doing the first calculations! Using MI object

--- src/MI_calculation.py
+++ src/MI_calculation.py
@@ -2,6 +2,8 @@
 import glob
 import logging
 import numpy as np
+import pandas as pd
+import os
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -10,75 +12,78 @@ from scipy.io import wavfile
 import scipy.signal as snl
 import processing as pr
 
+# Logging information to be able to debug
+logging.basicConfig(level=logging.DEBUG,
+                    format=" %(asctime)s - %(levelname)s - %(message)s")
+# logging.disable(logging.CRITICAL)
+
 # Define constants: frequency ranges, its limits and the limits of the bins for phase
+logging.debug("Define frequency ranges and phase limits for discretization.")
 freq_ranges = {"delta": (1, 3), "theta_low": (3, 5), "theta_high": (5, 7),
                "alpha": (7, 12), "beta": (12, 30), "gamma": (30, 50)}
 freq_limits = [0, 1, 3, 5, 7, 12, 30, 50]
 phs_lims = [-np.pi, -np.pi/2, 0, np.pi/2, np.pi]
 
 # Load segments path. Might change depending on where you put them directory-wise
-data_folder_path = "../DATA_TFM/EEG_Segmentado_filtrado_0.1-100_sin_artefactos/"
-segments_folder_path = data_folder_path + "segmentos/test/"
-segment_file_paths = glob.glob(segments_folder_path + "*.dat")
+raw_data_folder_path = "../raw_data/"
+data_folder_path = "../Data/"
+segment_file_paths = glob.glob(data_folder_path + "*.dat")
 
 # Speech files' paths
-multimedia_folder_path = "../DATA_TFM/Stimuli/"
+multimedia_folder_path = "../Data/Stimuli/"
 audio_file_paths = glob.glob(multimedia_folder_path + "*.wav")
 
 # Create dictionaries to contain the results of the calculations
-segments_processed = {"success": 0, "error": 0}
-mutual_information_sf = {"scrambled": {}, "face": {}, "scrambled_n": 0, "face_n": 0}
+mutual_information_sf = {"scrambled": {},
+                         "face": {}, "scrambled_n": 0, "face_n": 0}
 
+number_of_processed = 0
 # Loop over the segments in order to process them
-for sgmnt in segment_file_paths:
+logging.debug("Start iterating over segments \n \n")
+for sgmnt in segment_file_paths[:1000]:
+    logging.debug("-- Going over '" + sgmnt + "' --")
+
     # Load EEG and do some basic cleanup
-    segment = pr.Segment(sgmnt)
-    segment.dump_eyes()
-    segment.subtract_reference(rm_ref=False)
-    #segment.crop_audio_trial(inplace=True)
-    segment.bandpass_data(250, freq_ranges, orden=3)
+    logging.debug("Create MutualInformation object.")
+    mi = pr.MutualInformation(sgmnt, "phase", phs_lims, freq_ranges, "test")
 
     # Find the file with the speech of the trial
-    speech_audio_path = multimedia_folder_path + segment.audio_file
+    speech_audio_path = multimedia_folder_path + mi.segment.audio_file
 
     try:
-        assert(speech_audio_path in audio_file_paths)
+        assert(multimedia_folder_path +
+               mi.segment.audio_file in audio_file_paths)
     except:
-        print("Could not find audio file " + segment.audio_file)
+        print("Could not find audio file " + mi.segment.audio_file)
         input("Exitting now... (enter to quit)")
         exit()
 
+    logging.debug("Create AudioStim object.")
+    audio = pr.AudioStim(speech_audio_path)
+    audio.downsample(8)
+
     # Process and bin speech
-    fs, audio_data = wavfile.read(speech_audio_path)
-    inst_phase_aud = np.angle(snl.hilbert(audio_data))
-    hist_audio_phs, _ = np.histogram(inst_phase_aud, bins=phs_lims)
-
-    # Calculate MI per band per channel
-    mutualinfo = {}
-
-    for band in segment.bp_channels.keys():
-        mutualinfo[band] = {}
-        for channel, signal in segment.bp_channels[band].items():
-            an_signal = snl.hilbert(signal)
-            instphase = np.angle(an_signal)
-            histphase, _ = np.histogram(instphase, bins=phs_lims)
-
-            mutualinfo[band][channel] = pr.hist_mutual_info(
-                histphase, hist_audio_phs)
+    logging.debug("Compute mutual information.")
+    mi.compute(audio.data, audio.rate)
 
     # Add it to either face or scrambled distributions
-    kw = segment.is_face_kw()
+    kw = mi.segment.is_face_kw()
     kwn = kw + "_n"
 
     mutual_information_sf[kwn] += 1
     if mutual_information_sf[kw] == {}:
-        mutual_information_sf[kw] = mutualinfo
+        mutual_information_sf[kw] = mi.mi
     else:
-        for band in mutualinfo.keys():
-            for channel, value in mutualinfo[band].items():
-                    mutual_information_sf[kw][band][channel] += value
+        for band in mi.bands_dict.keys():
+            for channel, value in mi.mi[band].items():
+                mutual_information_sf[kw][band][channel] += value
+
+    number_of_processed += 1
+    logging.debug("Gone over " + str(number_of_processed /
+                                     len(segment_file_paths[:100])*100) + "% of files")
 
 # Calculate mean values for MI for each channel
+logging.debug("Starting to calculate mean values")
 kws = ["face", "scrambled"]
 for key in kws:
     kwn = key + "_n"
@@ -87,14 +92,23 @@ for key in kws:
             mutual_information_sf[key][band][channel] /= mutual_information_sf[kwn]
 
 # Find differences in MI between scrambled and non-scrambled
+logging.debug("Differences...")
 MI_diff = {}
 MI_rel = {}
 for band in mutual_information_sf["face"].keys():
     MI_diff[band] = {}
     MI_rel[band] = {}
     for channel, value in mutual_information_sf["face"][band].items():
-        MI_diff[band][channel] = value - mutual_information_sf["scrambled"][band][channel]
+        MI_diff[band][channel] = value - \
+            mutual_information_sf["scrambled"][band][channel]
         MI_rel[band][channel] = MI_diff[band][channel] / value
+
+with open("pleasesavethis.dat", "w") as thing:
+    thing.write(MI_diff)
+
+with open("pleasesavethisrelative.dat", "w") as thing:
+    thing.write(MI_rel)
+
 
 print("And the final results are...:")
 print(MI_diff)
